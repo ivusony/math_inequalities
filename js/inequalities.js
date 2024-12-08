b470b90d213d7ea14aadd60a22bdea429c2c46e1
ivan.radulov <ivan.radulov@sattrakt.com>
2024-12-08 17:34:23 +0100

    styles & calculateXrange update

--- js/inequalities.js
+++ js/inequalities.js
@@ -3,32 +3,56 @@ export default {
     calculateXrange
 }
 
-function calculateXrange( numberOfTasks, minNumber, maxNumber, containerElement ){
-    let userInputNumberOfTasks = prompt( "Koliko zadataka želiš da napraviš Kajo?" ) || numberOfTasks;
-    let operations = [ ">", "<" ];
-    function pickRandomOperation(){
-        return operations[ Math.floor( Math.random() * operations.length ) ];
-    }
-    function factory(){
-        let randomNumbers = [];
-        function numberGenerator( randomNumbers ){
-            let a = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-            if( !randomNumbers.includes( a ) ){
-                randomNumbers.push( a );
-            }
-            // console.log( randomNumbers );
-            if( randomNumbers.length === numberOfTasks ) return;
-            numberGenerator( randomNumbers );
-        }
-        numberGenerator( randomNumbers );
-
-        randomNumbers.forEach( number => {
-            let operation = pickRandomOperation();
-            let p = document.createElement( "p" );
-            p.textContent = `X ${operation} ${number}; X ∈ {___________________________}`;
-            containerElement.appendChild( p );
-            console.log( `X ${operation} ${number}; X ∈ {               }` );
-        } );
-    }
-    factory();
+let operations = [ ">", "<" ];
+function pickRandomOperation(){
+    return operations[ Math.floor( Math.random() * operations.length ) ];
 }
+
+// numberOfTasks: number of tasks to generate
+// minNumber: minimum number to generate - default 10
+// maxNumber: maximum number to generate - default 100
+// containerElement: element to append tasks to
+function calculateXrange( numberOfTasks, minNumber, maxNumber, containerElement ){
+    try{
+        let userInputNumberOfTasksRaw = prompt( "Koliko zadataka želiš da napraviš Kajo?" ) || numberOfTasks;
+        let userInputNumberOfTasks = parseInt( userInputNumberOfTasksRaw );
+        if( isNaN( userInputNumberOfTasks ) ){
+            console.error( "Unesi broj!" );
+            throw new Error( "Unesi broj!" );
+        }
+        function factory(){
+            let randomNumbers = [];
+            function numberGenerator( randomNumbers ){
+                let a = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+                if( !randomNumbers.includes( a ) ){
+                    randomNumbers.push( a );
+                }
+                // console.log( randomNumbers );
+                if( randomNumbers.length === userInputNumberOfTasks ) return;
+                numberGenerator( randomNumbers );
+            }
+            // generate numbers recursively
+            numberGenerator( randomNumbers );
+
+            //clear container
+            containerElement.innerHTML = "";
+
+            randomNumbers.forEach( ( number, idx ) => {
+                let operation = pickRandomOperation();
+                let p = document.createElement( "p" );
+                let span = document.createElement( "span" );
+                span.classList.add( "number" );
+                span.textContent = `${ idx + 1}.`;
+                p.prepend( span );
+                p.classList.add( "task" );
+                p.textContent = ` X ${operation} ${number}; X ∈ {___________________________}`;
+                containerElement.appendChild( p );
+            } );
+        }
+        factory();
+    }catch( e ){
+        console.error( e );
+        alert( e.message );
+    }
+}
+
